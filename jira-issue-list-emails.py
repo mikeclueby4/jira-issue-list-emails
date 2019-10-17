3c8a7ad852749acbcf21e12dc4e6ea05c24c04bb
mikeclueby4 <mikeclueby4@gmail.com>
2019-10-18 01:48:39 +0200

    html is produced, needs prettifying

--- jira-issue-list-emails.py
+++ jira-issue-list-emails.py
@@ -13,19 +13,27 @@ jira = JIRA(options = options, basic_auth = ("readonly", "NotSecret"), proxies =
 issues = jira.search_issues("project = COP AND created >= -7d", maxResults=1000)
 
 
+#
+# utils
+#
 
 import re
 def isearch(pattern, string):
     return re.search(pattern, string, re.IGNORECASE)
 
 
+#
+# scoring functions and patterns
+#
 
-# scoring of priorities
+
+# scoring of PRIORITIES
 prioscores = {}  # [id] = score
 priorities = jira.priorities()
 for idx,prio in enumerate(priorities):
     prioscores[prio.id] = ( len(priorities) - 1 - idx ) * 5     # 0, 5, 10, ...
 
+# scoring of STRINGS
 string_scorepatterns = {  # remember, several entry lines can match and will get added
     r'(vuln|attack|hack)': 7,
     r'(crash|hang|freeze)': 7,
@@ -34,6 +42,7 @@ string_scorepatterns = {  # remember, several entry lines can match and will get
     r'emergenc': 10
 }
 
+# scoring of LABELS
 label_scorepatterns = {  # remember, several entry lines can match and will get added
     r'support_need': 5,
     r'^support': 5,
@@ -49,7 +58,8 @@ def scorestring(string, patterns = None):
             ret += score
     return ret
 
-issuescores = {}
+
+# Loop through and score the returned issues
 
 for issue in issues:
     f = issue.fields
@@ -63,7 +73,164 @@ for issue in issues:
         li = getattr(link, "inwardIssue", None) or getattr(link, "outwardIssue", None)
         score += scorestring(li.fields.issuetype.name)
         score += scorestring(li.fields.summary)
+
+    if f.resolution and f.resolution.name.lower() in ["duplicate", "rejected", "invalid"]:
+        score = -1
+
     setattr(issue, "score", score)
 
+#
+# Build HTML!
+#
+
+from markupsafe import Markup, escape
+
+outs = []
+def out(str):
+    str = escape(str)
+    outs.append(str)    # instead of raw append on string = expensiiiive
+
+out(Markup(f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <base href="{options['server']}" target="_blank">
+    <style type="text/css">
+"""))
+out(Markup("""
+    li { padding: 10px; }
+    body {
+        font-family: BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Fira Sans","Droid Sans","Helvetica Neue",sans-serif;
+        font-size: 14px;
+    }
+    .description {
+        font-size: smaller;
+        padding-top: 8px;
+        display: inline-block;
+    }
+    .status {
+        border-radius: 3px;
+        display: inline-block;
+        font-weight: bold;
+        padding: 2px 5px;
+        text-align: center;
+        text-transform: uppercase;
+        box-sizing: border-box;
+        font-size: x-small;
+        background-color: #808080;
+        color: #ffffff;
+    }
+    .resolution {
+        font-size: x-small;
+    }
+    .subbox {
+        display: inline-block;
+        padding-left: 8em;
+    }
+
+    """))
+out(Markup(f"""
+    </style>
+</head>
+<body>
+<ul>
+"""))
+
 for issue in sorted(issues, key=lambda i: i.score, reverse=True):
-    print(issue.key, issue.score)
+    f = issue.fields
+    shortdesc = f.description[0:200].strip()
+    shortdesc = re.sub(r"(\r?\n)+", "\n", shortdesc)
+    shortdesc = Markup("<br>").join(shortdesc.split("\n")[0:3])
+    shortdesc += "..."
+    if not f.resolution:
+        resolution = ""
+    else:
+        resolution = f"Resolution: {f.resolution.name}"
+    out(Markup(f"""
+<li>
+<img src="{f.issuetype.iconUrl}" height="16" width="16" border="0" align="absmiddle" alt="{f.issuetype.name}">
+<a class="issue-link" href="/browse/{issue.key}">{issue.key}</a>
+<img src="{f.priority.iconUrl}" alt="{f.priority.name}" height="16" width="16" border"0" align="absmiddle">
+<span class="summary">{f.summary}</span>
+<br>
+<div class="subbox">
+<span class="status">{f.status.name}</span> &nbsp;
+<span class="resolution">{resolution}</span>
+<br>
+<span class="description">{shortdesc}</span></li>
+</div>
+"""))
+
+out(Markup(f"""
+</body>
+</html>
+"""))
+
+
+
+
+
+f = open("output.html", "w")
+for str in outs:
+    f.write(str)
+f.close()
+
+
+if False:
+
+
+    #
+    # Send email!
+    #
+
+    from email.message import EmailMessage
+    from email.utils import make_msgid
+    import mimetypes
+
+    msg = EmailMessage()
+
+    # generic email headers
+    msg['Subject'] = 'Hello there'
+    msg['From'] = 'ABCD <abcd@xyz.com>'
+    msg['To'] = 'PQRS <pqrs@xyz.com>'
+
+    # set the plain text body
+    msg.set_content('This is a plain text body.')
+
+    # now create a Content-ID for the image
+    image_cid = make_msgid(domain='xyz.com')
+    # if `domain` argument isn't provided, it will
+    # use your computer's name
+
+    # set an alternative html body
+    msg.add_alternative("""\
+    <html>
+        <body>
+            <p>This is an HTML body.<br>
+            It also has an image.
+            </p>
+            <img src="cid:{image_cid}">
+        </body>
+    </html>
+    """.format(image_cid=image_cid[1:-1]), subtype='html')
+    # image_cid looks like <long.random.number@xyz.com>
+    # to use it as the img src, we don't need `<` or `>`
+    # so we use [1:-1] to strip them off
+
+
+    # now open the image and attach it to the email
+    with open('path/to/image.jpg', 'rb') as img:
+
+        # know the Content-Type of the image
+        maintype, subtype = mimetypes.guess_type(img.name)[0].split('/')
+
+        # attach it
+        msg.get_payload()[1].add_related(img.read(),
+                                            maintype=maintype,
+                                            subtype=subtype,
+                                            cid=image_cid)
+
+
+    # the message is ready now
+    # you can write it to a file
+    # or send it using smtplib
